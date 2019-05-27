682cabbcdfd3039d6735ab3d41f3bd6a5c389f6e
Maksim Piskunov <maxitg@arachnid.attlocal.net>
2019-05-27 11:53:25 -0500

    Allows specifying pointCloudMultiplier as a build script argument.

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-./computeSimulations.wls &&
+pointCountMultiplier=1.0
+while getopts p: option
+do
+  case "${option}"
+    in
+    p) pointCountMultiplier=${OPTARG};;
+  esac
+done
+
+./computeSimulations.wls $pointCountMultiplier &&
 pdflatex coherent-enhancement.tex &&
 pdflatex coherent-enhancement.tex &&
 pdflatex coherent-enhancement.tex
