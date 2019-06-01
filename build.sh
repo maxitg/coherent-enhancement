5e6272f106e73f4b0ec51ffe827932aaefe681c1
Maksim Piskunov <maxitg@icloud.com>
2019-05-31 21:55:23 -0500

    Add option to pack for arXiv.

--- build.sh
+++ build.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
+usage() { cat << EOF
+usage: ./build.sh [-ah][-p <pointCountMultiplier>]
+
+  a : make .tar.gz suitable for arXiv upload
+  h : print this usage message
+
+  p multiplier : produce the paper with p times the normal number of points
+                 on scatter plots
+EOF
+exit 1;
+}
+
 pointCountMultiplier=1.0
-while getopts p: option
-do
-  case "${option}"
-    in
-    p) pointCountMultiplier=${OPTARG};;
+while getopts "p:ah" option; do
+  case "${option}" in
+    p)
+      pointCountMultiplier=${OPTARG};;
+    a)
+      createArxivFile=true;;
+    *)
+      usage;;
   esac
 done
+
+if [ "$createArxivFile" = true ] ; then
+  rm -rf figures
+fi
 
 ./computeSimulations.wls $pointCountMultiplier &&
 pdflatex coherent-enhancement.tex &&
 pdflatex coherent-enhancement.tex &&
 pdflatex coherent-enhancement.tex
+
+if [ "$createArxivFile" = true ] ; then
+  tar -cvzf coherent-enhancement-arxiv.tar.gz coherent-enhancement.tex figures
+fi
